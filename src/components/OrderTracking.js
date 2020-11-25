1b2d1ecbe02958a6eed1db2611eb886ae900ca97
Karolis Volungevičius <karolis.volungevicius@mif.stud.vu.lt>
2020-11-25 13:24:51 +0200

    added simple tracking

--- src/components/OrderTracking.js
+++ src/components/OrderTracking.js
@@ -3,42 +3,66 @@ import axios from 'axios';
 import '../Styles/BookList.css';
 
 
-
 const OrderTracking = () => {
+  const [trackingStatus, setTrackingStatus] = useState();
+  const [ID, setID] = useState('');
+  const [date, setDate] = useState('');
+  //console.log(ID);
 
+  const Searching = (event) => {
+    console.log(ID);
+
+    axios
+      .get('http://localhost:1337/trackings', {
+        params: {
+          TrackingID: ID,
+        },
+      })
+      .then(({ data }) => {
+        //console.log(data[0].Status);
+        setTrackingStatus(data);
+        let string = data[0].Date.toString();
+        let date1 = new Date(string);
+
+        let readableDate = date1.toUTCString();
+        setDate(readableDate);
+      })
+      .catch((e) => console.log(e));
+  };
 
   return (
-
     <section>
       <div className="row">
         <div className="col-lg-8">
           <div className="mb-3">
-            <div className="pt-4 wish-list">
-              <h5 className="mb-4">
-                Orders
-              </h5>
+            <div className="pt-4 wish-list inputDiv">
+              <h5 className="mb-4">Orders</h5>
               <input
-                type="email"
-                className="form-control"
+                id="ID"
+                className="inputDiv"
                 placeholder="Enter tracking id"
-                /*value={email}
-                onChange={(e) => setEmail(e.target.value)}*/
+                value={ID}
+                onChange={(e) => {
+                  setID(e.target.value);
+                }}
               />
-              <button>
+              <button className="inputDiv" onClick={Searching}>
                 Search
               </button>
-
-
             </div>
           </div>
+          {!trackingStatus ? (
+            ' '
+          ) : (
+            <div>
+              <p>Tracking ID: {trackingStatus[0].TrackingID}</p>
+              <p>Status: {trackingStatus[0].Status}</p>
+              <p>Order Date: {date} </p>
+            </div>
+          )}
         </div>
       </div>
     </section>
-
-
-
-
-
   );
 };
 export default OrderTracking;
