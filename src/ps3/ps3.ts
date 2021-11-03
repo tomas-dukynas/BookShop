e6e58f72279864c86e5e3cac0d4884edd0f1ca7a
Tomas Dukynas <tomasd@wix.com>
2021-11-03 17:13:07 +0200

    suggestions fixes

--- src/ps3/ps3.ts
+++ src/ps3/ps3.ts
@@ -4,20 +4,22 @@ const ps3 = () => {
   const [searchTerm, setSearchTerm] = React.useState('');
   const [searchResults, setSearchResults] = React.useState([]);
 
-  const handleChange = (event) => {
+  const handleInputChange = (event) => {
     setSearchTerm(event.target.value);
   };
 
+  // setting search results when user is typing
   React.useEffect(() => {
     const results = listBooks?.filter(
-      (person) =>
-        person.Author.toString().toLowerCase().includes(searchTerm.toLowerCase()) ||
-        person.NameOfTheBook.toString().toLocaleLowerCase().includes(searchTerm.toLowerCase()),
+      (book) =>
+        book.Author.toString().toLowerCase().includes(searchTerm.toLowerCase()) ||
+        book.NameOfTheBook.toString().toLocaleLowerCase().includes(searchTerm.toLowerCase()),
     );
 
     setSearchResults(results);
   }, [searchTerm]);
 
+  // setting book list
   React.useEffect(() => {
     if (
       searchTerm.length !== 0 &&
@@ -36,10 +38,7 @@ const ps3 = () => {
     ) {
       setBookList(listBooks);
     }
-    // console.log(authorsArray, 'AUTHORS ARRAY');
   }, [searchResults, searchTerm, categoriesArray, authorsArray]);
-
-  // console.log(categoriesArray, 'CATEG ARRAY');
 
   const PER_PAGE = 5;
   const [currentPage, setCurrentPage] = React.useState(0);
@@ -47,13 +46,13 @@ const ps3 = () => {
     setCurrentPage(selectedPage);
   }
 
-  const handleOnPress = (book) => {
-    console.log(book.id);
+  const onBookPress = (book) => {
     history.push('/one-book-view/' + book.id);
 
     if (book.PhotoOfTheBook?.name) {
       setImg(book.PhotoOfTheBook.name);
     } else {
+      // setting default img if book has no image
       setImg('https://www.liweddings.com/themes/default/assets/images/no-image.png');
     }
     setShow(true);
@@ -64,22 +63,4 @@ const ps3 = () => {
     });
     addComment(comments);
   };
-
-  if (
-    searchTerm.length !== 0 &&
-    listBooks !== bookList &&
-    searchResults !== bookList &&
-    searchResults.length === 0
-  ) {
-    setBookList(searchResults);
-  } else if (searchResults.length !== 0 && searchResults !== bookList) {
-    setBookList(searchResults);
-  } else if (
-    searchTerm.length === 0 &&
-    array.length === 0 &&
-    bookList.length === 0 &&
-    bookList !== listBooks
-  ) {
-    setBookList(listBooks);
-  }
 };
